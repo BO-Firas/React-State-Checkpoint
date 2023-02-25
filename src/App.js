cf707b151ee05e94a22befd501986dff717cfbbf
Mohamed Firas Ben Othmen <113718922+BO-Firas@users.noreply.github.com>
2023-02-25 04:24:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person: {
+      fullName: "Mohamed Firas Ben Othmen",
+      bio: "Student",
+      imgSrc: "/me.jpg",
+      profession: "Full Stack Developer"
+    },
+    shows: false,
+    intervalId: null,
+    mountedTime: null
+  };
+
+  toggleShows = () => {
+    this.setState(prevState => ({ shows: !prevState.shows }));
+  };
+
+  componentDidMount() {
+    const intervalId = setInterval(() => {
+      this.setState({ mountedTime: new Date() });
+    }, 1000);
+
+    this.setState({ intervalId, mountedTime: new Date() });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+    const { shows, mountedTime } = this.state;
+
+    return (
+      <div className="App">
+        <div className="card">
+          <h1>My Profile</h1>
+          <button onClick={this.toggleShows}>Toggle Profile</button>
+          {shows && (
+            <div>
+              <h2>{fullName}</h2>
+              <img src={imgSrc} alt={fullName} />
+              <p>{bio}</p>
+              <p>Profession: {profession}</p>
+            </div>
+          )}
+          <p>Component mounted {Math.floor((new Date() - mountedTime) / 1000)} seconds ago</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
